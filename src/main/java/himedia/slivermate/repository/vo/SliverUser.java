657b3d63727357bec4c3ab3cda0960c90ad81afb
김도형 <mrdos@naver.com>
2025-04-01 14:36:29 +0900

    [도형] 유저 정보 수정

--- src/main/java/himedia/slivermate/repository/vo/SliverUser.java
+++ src/main/java/himedia/slivermate/repository/vo/SliverUser.java
@@ -14,18 +14,20 @@ import lombok.ToString;
 @Setter
 @ToString
 public class SliverUser {
-	private Long uid;			// 유저 회원번호
+	private Long uid;				// 유저 회원번호
+	private Long group_id;			// 유저 그룹 아이디
+	private Integer user_type;		// 유저 타입 (1: 부모님1, 2: 부모님2, 3: 자식1, 4: 자식2) 
 	private String user_name;		// 이름
-	private String nickname;	// 닉네임
-	private String user_id;		// 유저 아이디
+	private String nickname;		// 닉네임
+	private String user_id;			// 유저 아이디
 	private String user_password;	// 비밀번호
-	private String tel_number;	// 전화번호
-	private String email;		// 이메일
-	private String thumbnail;	// 썸네일 주소
-	private Long gu_id;			// 구 정보
-	private Long recommend_uid;	// 추천인 회원번호
+	private String pin_password;	// pin 비밀번호
+	private String tel_number;		// 전화번호
+	private String email;			// 이메일
+	private String thumbnail;		// 썸네일 주소
+	private Long region_id;			// 지역번호
 	private Date register_date; 	// 가입일자
-	private Boolean is_deleted;	// 유저정보(탈퇴) 삭제 여부
-	private Boolean is_admin;	// 관리자 계정 여부
-	private Date upd_date;		// 갱신시간
+	private Boolean is_deleted;		// 유저정보(탈퇴) 삭제 여부
+	private Boolean is_admin;		// 관리자 계정 여부
+	private Date upd_date;			// 갱신시간
 }
