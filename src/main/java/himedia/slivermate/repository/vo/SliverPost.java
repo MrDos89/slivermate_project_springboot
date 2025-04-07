b3fe408b75b2c0f572e6f5a1ab2923baa57a7b18
김도형 <mrdos@naver.com>
2025-04-07 13:02:15 +0900

    [도형] 객체 데이터 수정

--- src/main/java/himedia/slivermate/repository/vo/SliverPost.java
+++ src/main/java/himedia/slivermate/repository/vo/SliverPost.java
@@ -17,15 +17,18 @@ import lombok.ToString;
 @ToString
 public class SliverPost {
 	private Long post_id;
+	private Long post_user_id;
 	private Long club_id;
 	private Integer region_id;
-	private Long category_id;
-	private Long sub_category_id;
+	private Long post_category_id;
+	private Long post_sub_category_id;
 	private String post_note;
 	private List<String> post_images; // MAP<LONG, STRING>
 	private Integer post_like_count;
 	private Integer post_comment_count;
 	private List<SliverComment> comments;
+	private boolean is_hidden;
+	private Integer post_report_count;
 	private Date register_date;
 	private Date upd_date;
 
