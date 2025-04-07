b83c0bb0d06d0b2a87f6f5c62d8c0219c750c3fe
김도형 <mrdos@naver.com>
2025-04-07 12:56:03 +0900

    [도형] get 맵핑 누락 추가

--- src/main/java/himedia/slivermate/controller/SliverPostController.java
+++ src/main/java/himedia/slivermate/controller/SliverPostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,8 @@ public class SliverPostController {
 	@Autowired
 	private SliverPostService sliverPostService;
 	
+	//	GET : /api/post
+	@GetMapping
 	public ResponseEntity<List<SliverPost>> selectAllPosts() {
 		List<SliverPost> posts = sliverPostService.selectAllPosts();
 		
