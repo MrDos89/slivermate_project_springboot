81f39a89922c171257ee489f5e5847cea17fdcaf
유예진 <yyj9290@naver.com>
2025-04-14 10:30:14 +0900

    [예진] 좋아요 수정중

--- src/main/java/himedia/slivermate/service/SliverLikeService.java
+++ src/main/java/himedia/slivermate/service/SliverLikeService.java
@@ -1,14 +1,12 @@
 package himedia.slivermate.service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import himedia.slivermate.repository.vo.SliverLike;
 import himedia.slivermate.mappers.SliverPostMapper;
+import himedia.slivermate.repository.vo.SliverLike;
 
 @Service
 public class SliverLikeService {
@@ -17,23 +15,38 @@ public class SliverLikeService {
 	private himedia.slivermate.mappers.SliverLikeMapper sliverLikeMapper;
 	@Autowired
 	private SliverPostMapper sliverPostMapper;
-	
+	@Autowired
+    private SliverPostService sliverPostService;
+    
 	// 좋아요 토글
 	public boolean toggleLike(SliverLike vo) {
-		int exists = sliverLikeMapper.exists(vo);
-		
-		if(exists == 0) {
-			return sliverLikeMapper.insertLike(vo) > 0;
-		} else {
-			boolean isLiked = sliverLikeMapper.isLiked(vo) > 0;
-			
-			if(isLiked) {
-				return sliverLikeMapper.deleteLike(vo) > 0;
-			} else {
-				return sliverLikeMapper.updateLike(vo) > 0;
-			}
-		}
+	    int exists = sliverLikeMapper.exists(vo);
+
+	    if (exists == 0) {
+	        boolean inserted = sliverLikeMapper.insertLike(vo) > 0;
+	        if (inserted) {
+	            sliverPostService.updatePostLikeCount((long) vo.getPost_id(), true);
+	        }
+	        return inserted;
+	    } else {
+	        boolean isLiked = sliverLikeMapper.isLiked(vo) > 0;
+
+	        if (isLiked) {
+	            boolean deleted = sliverLikeMapper.deleteLike(vo) > 0;
+	            if (deleted) {
+	                sliverPostService.updatePostLikeCount((long) vo.getPost_id(), false);
+	            }
+	            return deleted;
+	        } else {
+	            boolean updated = sliverLikeMapper.updateLike(vo) > 0;
+	            if (updated) {
+	                sliverPostService.updatePostLikeCount((long) vo.getPost_id(), true);
+	            }
+	            return updated;
+	        }
+	    }
 	}
+
 	
 
 	public List<Integer> getLikedPostIds(int user_id) {
