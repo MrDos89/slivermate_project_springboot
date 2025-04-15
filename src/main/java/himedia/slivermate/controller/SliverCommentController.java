c582d9a2f35cb7e7dcfcc3175d42302054fbc95a
김도형 <mrdos@naver.com>
2025-04-15 23:25:59 +0900

    [도형] 빠진 문법 추가

--- src/main/java/himedia/slivermate/controller/SliverCommentController.java
+++ src/main/java/himedia/slivermate/controller/SliverCommentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,7 @@ public class SliverCommentController {
 	}
 	
 	@GetMapping("/u/{uid}")
-	public ResponseEntity<List<SliverComment>> selectMyComments(Long uid) {
+	public ResponseEntity<List<SliverComment>> selectMyComments(@PathVariable Long uid) {
 		List<SliverComment> comments = sliverCommentService.selectMyComments(uid);
 		
 		return ResponseEntity.ok(comments);
